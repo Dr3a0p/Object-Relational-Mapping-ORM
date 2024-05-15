8d898772f07735826489ec675cdaca6d24d1d55e
Andrea Castillo <dreandreax@icloud.com>
2024-05-14 20:39:05 -0500

    will come back to this

--- develop/routes/index.js
+++ develop/routes/index.js
@@ -8,3 +8,16 @@ router.use((req, res) => {
 });
 
 module.exports = router;
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
